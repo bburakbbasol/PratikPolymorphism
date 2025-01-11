2e86c6e1205aea71a93b96eaaab331bfe03ac81f
bburakbbasol <bburakbbasol@gmail.com>
2025-01-11 16:47:56 +0300

    Add project files.

---
+++ PratikPolymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PratikPolymorphism
+{
+	// Temel geometrik şekil sınıfı
+	public class BaseGeometrikSekil
+	{
+		private int _uzunluk;
+		private int _genislik;
+
+		// Uzunluk özelliği
+		public int Uzunluk
+		{
+			get { return _uzunluk; }
+			set
+			{
+				if (value > 0)
+				{
+					_uzunluk = value;
+				}
+				else
+				{
+					Console.WriteLine("Sıfırdan büyük olmalı");
+				}
+			}
+		}
+
+		// Genişlik özelliği
+		public int Genislik
+		{
+			get { return _genislik; }
+			set
+			{
+				if (value > 0)
+				{
+					_genislik = value;
+				}
+				else
+				{
+					Console.WriteLine("Sıfırdan büyük olmalı");
+				}
+			}
+		}
+
+		// Sanal alan metodu
+		public virtual void Alan()
+		{
+		}
+	}
+
+	// Kare sınıfı, BaseGeometrikSekil'den türetilir
+	public class Kare : BaseGeometrikSekil
+	{
+		public override void Alan()
+		{
+			Console.WriteLine($"Karenin alanı: {Uzunluk * Genislik}");
+		}
+	}
+
+	// Dikdörtgen sınıfı, BaseGeometrikSekil'den türetilir
+	public class Diktortgen : BaseGeometrikSekil
+	{
+		public override void Alan()
+		{
+			if (Uzunluk == Genislik)
+			{
+				Console.WriteLine($"Diktörgenin alanı: {Uzunluk * Genislik}");
+			}
+			else
+			{
+				Console.WriteLine("Karenin uzunluğu ve genişliği eşit olmalı");
+			}
+		}
+	}
+
+	// Üçgen sınıfı, BaseGeometrikSekil'den türetilir
+	public class Ucgen : BaseGeometrikSekil
+	{
+		public override void Alan()
+		{
+			Console.WriteLine($"Üçgenin alanı: {(Uzunluk * Genislik) / 2}");
+		}
+	}
+}
---
+++ PratikPolymorphism/Program.cs
@@ -0,0 +1,16 @@
+﻿using PratikPolymorphism;
+
+BaseGeometrikSekil ucgen1 = new Ucgen();
+ucgen1.Uzunluk = 45;
+ucgen1.Genislik = 55;
+BaseGeometrikSekil kare1=new Kare();
+kare1.Uzunluk = 35;
+kare1.Genislik= 33;
+Diktortgen diktortgen = new Diktortgen();
+diktortgen.Uzunluk = 41;
+diktortgen.Genislik = 55;
+diktortgen.Alan();
+ucgen1.Alan();
+kare1.Alan();
+
+
